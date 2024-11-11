639e784161a5d9fa8ea1a3eaba7b9541d76e992d
victor <109164737+VictorrOG19@users.noreply.github.com>
2024-11-11 21:40:25 +0000

    New changes: Navbar

--- portfolio/src/app/components/Navbar.tsx
+++ portfolio/src/app/components/Navbar.tsx
@@ -1,4 +1,6 @@
 import styles from "../styles/Navbar.module.css";
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export default function Navbar() {
     return (
@@ -8,10 +10,15 @@ export default function Navbar() {
                 <a href="/">Victor Natan</a></div>
 
             <div className={styles.links}>
-                <a href="/">Inicio</a>
+                <a href="/">Início</a>
                 <a href="/">Sobre mim</a>
                 <a href="/">Contato</a>
             </div>
+
+            <div className={styles["mobile-button"]}>
+                <button>
+                    <FontAwesomeIcon icon={faPlus}></FontAwesomeIcon>
+                </button>
+            </div>
         </div>
     );
-}

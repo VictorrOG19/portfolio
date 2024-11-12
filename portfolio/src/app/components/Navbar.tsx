437e361bf467190e6d841981d406acc183371577
victor <109164737+VictorrOG19@users.noreply.github.com>
2024-11-12 17:43:33 +0000

    New changes: Update Navbar

--- portfolio/src/app/components/Navbar.tsx
+++ portfolio/src/app/components/Navbar.tsx
@@ -1,24 +1,43 @@
+"use client"
+import { useState } from "react";
 import styles from "../styles/Navbar.module.css";
-import { faPlus } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Plus } from "lucide-react";
 
 export default function Navbar() {
+    const [isOpen, setOpen] = useState(false);
+
     return (
-        <div className={styles.navbar}>
-            <div className={styles.logo}>
-                <div className={styles.circle}></div>
-                <a href="/">Victor Natan</a></div>
+        <>
+            <div className={styles.navbar}>
+                <div className={styles.logo}>
+                    <div className={styles.circle}></div>
+                    <a href="/">Victor Natan</a>
+                </div>
 
-            <div className={styles.links}>
-                <a href="/">Início</a>
-                <a href="/">Sobre mim</a>
-                <a href="/">Contato</a>
-            </div>
+                <div className={styles.links}>
+                    <a href="/">Início</a>
+                    <a href="/">Sobre mim</a>
+                    <a href="/">Contato</a>
+                </div>
 
-            <div className={styles["mobile-button"]}>
-                <button>
-                    <FontAwesomeIcon icon={faPlus}></FontAwesomeIcon>
-                </button>
+                <div className={styles["mobile-button"]}>
+                    <button
+                        className={`${styles.icon} ${isOpen ? styles.rotate : ''}`}
+                        onClick={() => setOpen(!isOpen)}
+                    >
+                        <Plus />
+                    </button>
+                </div>
             </div>
-        </div>
+            {isOpen && (
+                <div className={styles["mobile-menu"]}>
+                    <div className={styles["links-mobile"]}>
+                        <a href="/">Início</a>
+                        <a href="/">Sobre mim</a>
+                        <a href="/">Contato</a>
+                    </div>
+                </div>
+            )}
+        </>
     );
+}
